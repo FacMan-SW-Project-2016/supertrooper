d219473f10f61fed35c832267b01e41705750e2f
FelixGillissen <Felix.gillissen@SAP.com>
2016-10-22 20:02:10 +0200

    dynamic table mit bootstrap geht, UI aufgefrischt und refresh und zurück taste eingebaut

--- website/js/bootstraptable_controller.js
+++ website/js/bootstraptable_controller.js
@@ -1,11 +1,40 @@
 
 $('#table').bootstrapTable({
-    url: 'http://localhost:8000/room/',
-    columns: [{
-        field: 'ID',
-        title: 'Item ID'
+    url: 'http://localhost:8000/report/',
+    columns: [     
+        {
+        field: 'remove',
+            title: 'Remove',
+        formatter: actionFormatter,
+            events:deleteReport
     }, {
-        field: 'name',
-        title: 'Item Name'
-    }, ]
+        field: 'title',
+        title: 'Titel'
+    },{
+        field: 'room',
+        title: 'Raum'
+    },{
+        field:'category',
+        title:'Kategorie'
+    },{
+        field:'moment',
+        title:'Zeit'
+    }
+             ]
 });
+$('#table').on('click-row.bs.table', function (e, row, $element) {
+        console.log('Event: click-row.bs.table ID:' + row.ID);
+    });
+
+
+
+function actionFormatter(value, row, index){
+    return [
+        '<a class="remove ml10" href="javascript:void(0)" title="Remove">',
+        '<i class="remove icon"></i>',
+        '</a>'
+    ].join('');
+}
+function deleteReport(value, row, index){
+    alert('delete triggered for row ID ' + row.ID); 
+}
