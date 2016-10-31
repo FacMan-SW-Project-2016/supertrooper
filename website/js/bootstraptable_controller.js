5d6e800765cc44d020fc16078d35908e3a963882
D062641 <D062641@WDFN00352511A.emea.global.corp.sap>
2016-10-31 12:14:14 +0100

    woraround dropdown

--- website/js/bootstraptable_controller.js
+++ website/js/bootstraptable_controller.js
@@ -1,7 +1,7 @@
 
 $('#table').bootstrapTable({
     url: 'http://localhost:8000/report/',
-    columns: [     
+    columns: [
         {
         field: 'remove',
             title: 'Remove',
@@ -22,36 +22,63 @@ $('#table').bootstrapTable({
     }
              ]
 });
+
+
+function setDynamicOptions(selector, options) {
+    var att = "data-dinamic-opt";
+    $(selector).find('[' + att + ']').remove();
+    var html;
+    for(key in options) {
+
+    if (html != undefined)
+    {
+      html += '<div class="item" data-value="' + options[key].ID + '">' + options[key].text + '</div>';
+    }else {
+      html = '<div class="item" data-value="' + options[key].ID + '">' + options[key].text + '</div>';
+    }
+  }
+
+    $(selector + ' .menu').html(html);
+    $(selector).dropdown({showOnFocus: false});
+}
+
 $('#table').on('click-row.bs.table', function (e, row, $element) {
         console.log('Event: click-row.bs.table ID:' + row.ID);
     var time = row.moment.substring(0,16);
-    
+
     $('#popupTitle').text(row.title  + " - " + time);
     $('#popupPic').attr('src', '');
     if(row.data!==null){
     $('#popupPic').attr('src', 'http://localhost:8000/photo/download/' + row.ID);
     }
-    
+
     $('#popupID').text(row.ID);
     $('#popupRoom').text(row.room);
-    $('#popupCat').text(row.category);
+
+    $('#dropdownCat').dropdown({showOnFocus: false});
+
+    $.ajax({
+  		url : 'http://localhost:8000/category/',
+  		cache : false,
+  		type : 'GET',
+  		success : function (data) {
+
+           setDynamicOptions('#dropdownCat', data.results);
+       },
+  		async : false
+  	});
+
     $('#popupStatus').text(row.status);
     $('#popupUser').text(row.usercreate);
     $('#popupDesc').text(row.description);
     $('#popupFacMan').text(row.userfacman);
+
+    $('#dropdownCat').dropdown('set selected', row.category);
     $('.ui.modal').modal('show');
 
     });
 
-$('#dropdownCat').dropdown({
-    onClick : function (value) {
-        
-    },
-    apiSettings:{
-    url: '//localhost:8000/category'//,
-    //cache: false
-}
-});
+
 
 
 
@@ -64,5 +91,5 @@ function actionFormatter(value, row, index){
     ].join('');
 }
 function deleteReport(value, row, index){
-    alert('delete triggered for row ID ' + row.ID); 
+    alert('delete triggered for row ID ' + row.ID);
 }
