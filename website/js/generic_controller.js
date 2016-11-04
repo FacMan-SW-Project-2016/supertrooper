110c0f2aa8a09217e4771c3f5325bb4a40a315f8
nicolasknauber <nicolas.knauber@sap.com>
2016-11-04 10:59:11 +0100

    The log off function now deletes all cookies

--- website/js/generic_controller.js
+++ website/js/generic_controller.js
@@ -1,28 +1,32 @@
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
 }
 
 function checkCookie() {
-    var user = getCookie("username");
-    if (user != "") {
-    	$('#info_container').html('<i class="close icon"></i>Willkommen zurück, ' + user + '.');
-    	// Fill the #whoami div with content
-    	$('#whoami').html('<div class="item">Sie sind angemeldet als ' + user + '.</div><a href="/login.html">  Abmelden</a>');
-    } else {
-    	window.alert('Bitte loggen Sie sich ein, bevor Sie diese Seite aufrufen.');
-        window.location.href='/login.html';
-    }
+	var user = getCookie("username");
+	if (user != "") {
+		$('#info_container').html(
+				'<i class="close icon"></i>Willkommen zurück, ' + user + '.');
+		// Fill the #whoami div with content
+		$('#whoami').html(
+				'<div class="item">Sie sind angemeldet als ' + user
+						+ '.</div><a href="/login.html" onClick="deleteAllCookies()">  Abmelden</a>');
+	} else {
+		window
+				.alert('Bitte loggen Sie sich ein, bevor Sie diese Seite aufrufen.');
+		window.location.href = '/login.html';
+	}
 }
 
 function setCookie(cname, cvalue, exdays) {
@@ -32,30 +36,30 @@ function setCookie(cname, cvalue, exdays) {
 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-//selector cache
-var     $menu                = $('#menu');
+function deleteAllCookies() {
+	document.cookie.split(";").forEach(
+			function(c) {
+				document.cookie = c.replace(/^ +/, "").replace(/=.*/,
+						"=;expires=" + new Date().toUTCString() + ";path=/");
+			});
+}
 
+// selector cache
+var $menu = $('#menu');
 
 // main sidebar
-$menu
- .sidebar({
-   dimPage          : true,
-   transition       : 'overlay',
-   mobileTransition : 'uncover'
- })
-;
+$menu.sidebar({
+	dimPage : true,
+	transition : 'overlay',
+	mobileTransition : 'uncover'
+});
 
 // launch buttons
-$menu
- .sidebar('attach events', '.launch.button, .view-ui, .launch.item')
-;
+$menu.sidebar('attach events', '.launch.button, .view-ui, .launch.item');
 
-$('.ui.sticky')
-.sticky({
-  context: '#reportForm'
-})
-;
+$('.ui.sticky').sticky({
+	context : '#reportForm'
+});
 
 // Call the checkCookie method for all the sites that bind this .js
 checkCookie();
-
